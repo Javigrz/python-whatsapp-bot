acd826ab2165071ead9d89e7da5ec5af06a5267a
Javigrz <jaavii.grz@gmail.com>
2025-05-27 22:12:54 +0200

    proyecto funcionando

--- run.py
+++ run.py
@@ -8,3 +8,6 @@ app = create_app()
 if __name__ == "__main__":
     logging.info("Flask app started")
     app.run(host="0.0.0.0", port=8000)
+
+
+# ngrok http 8000 --domain loon-enhanced-cougar.ngrok-free.app
